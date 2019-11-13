97526e39ab86a58ef6744639da06efd58c199b9c
Паша <holms703@gmail.com>
2019-11-13 05:20:36 +0200

    hw013

--- JavaScript/hw-01/task-3/js/index.js
+++ JavaScript/hw-01/task-3/js/index.js
@@ -3,16 +3,13 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
 let invite = prompt('Введите пароль', 'пароль');
-console.log(message, invite);
+
 if (invite === null) {
   message = 'Отменено пользователем!';
-  console.log(message, invite);
 } else if (invite === ADMIN_PASSWORD) {
   message = 'Добро пожаловать!';
-  console.log(message, invite);
 } else {
   message = 'Доступ запрещен, неверный пароль!';
-  console.log(message, invite);
 }
-console.log(message, invite);
+
 alert(message);
