53e588875f8ebd01f0d8655863d6649cdd69bda8
Паша <holms703@gmail.com>
2019-11-15 01:27:00 +0200

    input!==null

--- JavaScript/hw-01/task-6/js/index.js
+++ JavaScript/hw-01/task-6/js/index.js
@@ -2,16 +2,25 @@
 
 let input;
 let total = 0;
-while (true) {
+console.log(input, total);
+while (input !== null) {
+  console.log(input, total);
   input = prompt('Введите число', '');
+  console.log(input, total);
   if (input === null) {
+    console.log(input, total);
     alert(`Общая сумма чисел равна ${total}`);
     break;
   }
+  console.log(input, total);
   input = Number(input);
   if (Number.isNaN(input)) {
+    console.log(input, total);
     alert('Было введено не число,попробуйте еще раз');
     continue;
+    console.log(input, total);
   }
   total += Number(input);
+  console.log(input, total);
 }
+console.log(input, total);
