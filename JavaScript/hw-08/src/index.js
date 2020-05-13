7bb2ec380d4cc5a9ac95b7ec6992d1e34a948bec
Паша <holms703@gmail.com>
2020-05-13 22:13:13 +0300

    fixed hw-08

--- JavaScript/hw-08/src/index.js
+++ JavaScript/hw-08/src/index.js
@@ -1,56 +1,54 @@
-"use strict";
-import images from "./gallery-items.js";
+'use strict';
+import images from './gallery-items.js';
 
-const image = document.createElement("img");
-const gallery = document.querySelector(".js-gallery");
-const lightbox = document.querySelector(".lightbox");
-const closeButton = document.querySelector('[data-action="close-lightbox"]');
-const modal = document.querySelector(".lightbox__content");
-const lightboxImage = document.querySelector(".lightbox__image");
+const refs = {
+	gallery: document.querySelector('.js-gallery'),
+	lightbox: document.querySelector('.lightbox'),
+	closeButton: document.querySelector('[data-action="close-lightbox"]'),
+	modal: document.querySelector('.lightbox__content'),
+	lightboxImage: document.querySelector('.lightbox__image'),
+};
 
 const galleryItem = ({ preview, original, description }) =>
-  `<li class="gallery__item">
+	`<li class="gallery__item">
 <a class="gallery__link" href=${original}> <img class="gallery__image"
     src=${preview} data-source=${original} alt=${description}/></a></li>`;
-const allGallery = images.reduce((acc, item) => acc + galleryItem(item), "");
-gallery.insertAdjacentHTML("afterbegin", allGallery);
 
-image.classList.add("gallery__image");
+const allGallery = images.reduce((acc, item) => acc + galleryItem(item), '');
 
-gallery.addEventListener("click", galleryClick);
-
-modal.addEventListener("click", closeClick);
-
-closeButton.addEventListener("click", closeLightbox);
+refs.gallery.insertAdjacentHTML('afterbegin', allGallery);
+refs.gallery.addEventListener('click', galleryClick);
+refs.modal.addEventListener('click', closeClick);
+refs.closeButton.addEventListener('click', closeLightbox);
 
 function galleryClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== "IMG") {
-    return;
-  }
-  if (e.target.nodeName === "IMG") {
-    lightbox.classList.add("is-open");
-    lightboxImage.src = e.target.getAttribute("data-source");
-    lightboxImage.alt = e.target.alt;
-  }
-  window.addEventListener("keyup", closePressKey);
+	e.preventDefault();
+	if (e.target.nodeName !== 'IMG') {
+		return;
+	}
+	if (e.target.nodeName === 'IMG') {
+		refs.lightbox.classList.add('is-open');
+		refs.lightboxImage.src = e.target.getAttribute('data-source');
+		refs.lightboxImage.alt = e.target.alt;
+	}
+	window.addEventListener('keyup', closePressKey);
 }
 
 function closeLightbox(e) {
-  lightbox.classList.remove("is-open");
-  lightboxImage.src = "";
-  lightboxImage.alt = "";
-  window.removeEventListener("keyup", closePressKey);
+	refs.lightbox.classList.remove('is-open');
+	refs.lightboxImage.src = '';
+	refs.lightboxImage.alt = '';
+	window.removeEventListener('keyup', closePressKey);
 }
 
 function closeClick(click) {
-  if (click.target === click.currentTarget) {
-    closeLightbox();
-  }
+	if (click.target === click.currentTarget) {
+		closeLightbox();
+	}
 }
 
 function closePressKey(pressKey) {
-  if (pressKey.code === "Escape") {
-    closeLightbox();
-  }
+	if (pressKey.code === 'Escape') {
+		closeLightbox();
+	}
 }
