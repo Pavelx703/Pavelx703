fa4ebf663c6cd1c2150cf421facf142c0ec64398
Паша <holms703@gmail.com>
2020-05-25 22:50:28 +0300

    empty line check

--- JavaScript/hw-12/src/js/query.js
+++ JavaScript/hw-12/src/js/query.js
@@ -17,33 +17,33 @@ function countrySearchInputHandler(e) {
   e.preventDefault();
   clearArticlesContainer();
   const queryValue = e.target.value;
-
-  countrySearch
-    .fetchArticles(queryValue)
-    .then(data => {
-      if (data.length > 10) {
-        error({
-          text: 'Too many matches found. Please enter a more specific query!',
+  if (queryValue) {
+    countrySearch
+      .fetchArticles(queryValue)
+      .then(data => {
+        if (data.length > 10) {
+          error({
+            text: 'Too many matches found. Please enter a more specific query!',
+          });
+        } else if (data.status === 404) {
+          error({
+            text:
+              'No country has been found. Please enter a more specific query!',
+          });
+        } else if (data.length === 1) {
+          buildListMarkup(data, articlesOneCountry);
+        } else if (data.length <= 10) {
+          buildListMarkup(data, countryList);
+        }
+      })
+      .catch(Error => {
+        Error({
+          text: 'You must enter query parameters!',
         });
-      } else if (data.status === 404) {
-        error({
-          text:
-            'No country has been found. Please enter a more specific query!',
-        });
-      } else if (data.length === 1) {
-        buildListMarkup(data, articlesOneCountry);
-      } else if (data.length <= 10) {
-        buildListMarkup(data, countryList);
-      }
-    })
-    .catch(Error => {
-      Error({
-        text: 'You must enter query parameters!',
+        console.log(Error);
       });
-      console.log(Error);
-    });
+  }
 }
-
 function buildListMarkup(countryes, template) {
   const markup = countryes.map(count => template(count)).join();
   refs.articlesContainer.insertAdjacentHTML('afterbegin', markup);
