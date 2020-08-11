2bab2205bca94f809b55ef6bffee531abd0e5c64
Паша <holms703@gmail.com>
2020-08-11 20:19:10 +0300

    Updates

---
+++ precache-manifest.b8683b8da5e75e6314d7bc63c9a349d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113470a73ba5fc4756376cce396b3010",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b235634063ae85e37ddf",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d732b9c8cc04c439882c",
+    "url": "/test/static/js/2.97dc257e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.97dc257e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b235634063ae85e37ddf",
+    "url": "/test/static/js/main.315f837a.chunk.js"
+  },
+  {
+    "revision": "88d1338c3e3aad443233",
+    "url": "/test/static/js/runtime-main.0979ccd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
